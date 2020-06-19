36f28ce19d32e3c85f22079ee05190d319aca067
dvlprsh <dvlprsh103@gmail.com>
2020-06-20 07:09:22 +0900

    py: Solve heap (improve efficiency)

--- Programmers/코딩테스트 고득점 Kit/Heap/디스크 컨트롤러(lv3).py
+++ Programmers/코딩테스트 고득점 Kit/Heap/디스크 컨트롤러(lv3).py
@@ -1,5 +1,35 @@
 # Solve
-#풀이 1 - 100점 (heap 정렬기준 - 작업 소요시간)
+#풀이1 (풀이2 효율성 개선) 100점 (heap 정렬기준 - 작업 소요시간)
+import heapq
+from collections import deque
+def set_duplicate(last):
+    # last 보다 요청시점 빠른 작업들 duplicate에 할당 -> 소요시간 오름차순으로 처리
+    while _jobs:
+        if _jobs[0][0] < last:
+            s, d = _jobs.popleft()
+            heapq.heappush(_duplicate, (d, s))
+        else:
+            break
+    return
+def solution(jobs):
+    answer = 0
+    global _jobs, _duplicate
+    _jobs = deque(sorted(jobs, key = lambda x: (x[0], x[1])))
+    _duplicate = [] # (0: 소요시간, 1: 요청시점)
+    while _jobs:
+        start, duration = _jobs.popleft()
+        last = start + duration
+        answer += duration
+        set_duplicate(last)
+        
+        while _duplicate:
+            d, s = heapq.heappop(_duplicate)
+            answer += last - s + d
+            last = last + d
+            set_duplicate(last)
+    return answer // len(jobs)
+    
+#풀이 2 - 100점 (heap 정렬기준 - 작업 소요시간)
 import heapq
 def solution(jobs):
     answer = 0
@@ -33,7 +63,7 @@ def solution(jobs):
     return answer // num
 
 
-#풀이 2 - 35점 (heap 정렬기준 - 대기시간 + 작업소요시간)
+#풀이 3 - 35점 (heap 정렬기준 - 대기시간 + 작업소요시간)
 import heapq
 def solution(jobs):
     answer = 0
